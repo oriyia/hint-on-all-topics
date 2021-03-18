13c5189c7be411ed99a94b9310fee91c7e44b077
Ilya Yarusov <originalyia@gmail.com>
2021-03-18 17:29:42 +0400

    update info for "Fisher_exact"

--- SciPy/scipy_start.py
+++ SciPy/scipy_start.py
@@ -5,6 +5,7 @@ import numpy as np
 import plotly.express as px
 import plotly.figure_factory as ff
 import plotly.offline as offline
+import sklearn
 
 # задание нормального распределения
 np.random.seed(0)  # - для какой-то там нормализации
@@ -40,6 +41,62 @@ def create_distplot():
 # a = chisquare([795, 705])  # вывод Power_divergenceResult(statistic=5.4, p-value=0.02013675155034633)
 # Cumulative distribution function (cdf) - ФР
 # a = scipy.stats.chi2.cdf(2, 2)  # p-value при df=2, со значением хи2=2
+# более подробная информация
+# observed = [[10, 6], [5, 15]]
+# chi2, p, df, exp = chi2_contingency(observed,
+#                                     correction=False) - поправка Йетса
 
+# Точный критерий фишера
+# observed = [[1, 3], [3, 1]]
+# result = scipy.stats.fisher_exact(observed)
+
+'''
+Scikit-Learn - ДОКУМЕНТАЦИЯ
+Чаще всего использование API статистического оценивания библиотеки Scikit-Learn включает следующие шаги (далее мы 
+рассмотрим несколько подробных примеров).
+1. Выбор класса модели с помощью импорта соответствующего класса оценивателя из библиотеки Scikit-Learn.
+2. Выбор гиперпараметров модели путем создания экземпляра этого класса с соответствующими значениями.
+3. Компоновка данных в матрицу признаков и целевой вектор в соответствии с описанным выше.
+4. Обучение модели на своих данных посредством вызова метода fit() экземпляра модели.
+5. Применение модели к новым данным:
+- в случае машинного обучения с учителем метки для неизвестных данных обычно предсказывают с помощью метода predict();
+- в случае машинного обучения без учителя выполняется преобразование свойств данных или вывод их значений посредством 
+  методов transform() или predict().
+'''
+# разделение данных на обучающую выборку (training set) и контрольную (testing set)
+from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y,
+#                                                     random_state=42,  # управляет перемешиванием
+#                                                     train_size=0.33)  # размер тренировочной выборки
+
+# линейная регрессия
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_boston
+# загрузка данных
+boston = load_boston()
+features = boston.data[:, 0:2]  # только 2 предиктора (признака)
+target = boston.target
+# создать объект (экземпляр класса) линейной регрессии
+regression = LinearRegression()
+# подгонка линейной регрессии
+# model = regression.fit(X_train, y_train)
+# print(model.intercept_)  # вывод intercept
+# print(model.coef_)  # вывод коэффициентов
+# предсказание
+# y_pred = model.predict(X_test)
+# Оценка эффективности модели (оценка точности)
+from sklearn.metrics import accuracy_score
+# 1 способ - тестовые данные у
+# score = accuracy_score(y_true=y_test, y_pred=y_pred)
+# 2 способ - перекрестная проверка модели (cross validation)
+from sklearn.model_selection import cross_val_score
+# score = cross_val_score(model, x, y, cv=5)
+
+
+
+# Логистическая регрессия
+from sklearn.linear_model import LogisticRegression
+# logreg = LogisticRegression()
+# model = logreg()
 
 # offline.plot(create_distplot())
