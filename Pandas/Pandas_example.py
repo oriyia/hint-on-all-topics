43efea93390fcb14bd4618f30cb0d801d63b106d
Ilya Yarusov <originalyia@gmail.com>
2021-05-14 12:44:26 +0800

    completion of generalization "Pandas"

--- Pandas/Pandas_example.py
+++ Pandas/Pandas_example.py
@@ -2,9 +2,4 @@ import numpy as np
 import pandas as pd
 
 
-arr = pd.DataFrame([[1, 2, 3], [1, 2, 3]])
-print(arr)
-arr.columns = ['hfp', 'fa', 'asdf']
-print(arr)
-arr.columns = ['asdf', 'asdfas', 'sdafdas']
-print(arr)
+# ДОБАВЬ ЖЕ ЧТО-НИБУДЬ ПОЛЕЗНОЕ
