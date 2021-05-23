a20910f8a1022b36e63f69ab9bcd03038d1eb40b
Ilya Yarusov <originalyia@gmail.com>
2021-05-23 19:49:13 +0800

    add new file "sql_start.py"

--- Pandas/Pandas_example.py
+++ Pandas/Pandas_example.py
@@ -3,3 +3,7 @@ import pandas as pd
 
 
 # ДОБАВЬ ЖЕ ЧТО-НИБУДЬ ПОЛЕЗНОЕ
+df = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=['one', 'two', 'three'], index=['a', 'b', 'c'])
+print(df)
+print(df.iloc[1, 2])
+print(df[['a', 'b']][['one', 'two']])
